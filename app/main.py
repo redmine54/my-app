291360d648e83eb3d5da8e65dae7d1d315d41c22
redomine54 <hiroji.redmine@gmail.com>
2026-03-28 21:05:38 +0900

    CI try17 pytest

--- app/main.py
+++ app/main.py
@@ -4,4 +4,8 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Welcome"}
+    return {"message": "Hello FastAPI!"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str | None = None):
+    return {"item_id": item_id, "q": q}
